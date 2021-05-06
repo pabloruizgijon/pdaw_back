c122ee85be6e1055b2dfe434feedecb4a1887702
Pablo Lopez Burgos <pablo.lopez.external@atos.net>
2021-05-06 18:58:13 +0200

    back

---
+++ src/main/java/com/pablo/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package com.pablo.springboot.backend.apirest.models.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pablo.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends JpaRepository<Cliente, Long>{
+
+}
